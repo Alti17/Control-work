1a24a3e860a5d0e218036f380a112c5f0ed8e236
Romanyuk <sunrise15@mail.ru>
2022-07-30 01:35:50 +0400

    array generation method created

--- program/Program.cs
+++ program/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] CreateStringArray(int size)
+{
+    string[] words=new string[size];
+    for(int i=0; i<size; i++)
+    {
+        Console.Write($"Input {i+1} word: ");
+        words[i] = Console.ReadLine();
+    }
+    return words;
+}
